1f0cb18bb94d1f973016b38a15444c96f3ed73e6
lebrick07 <85870320+lebrick07@users.noreply.github.com>
2025-02-14 15:34:52 -0500

    Changing ports

--- modules/eks/sg.tf
+++ modules/eks/sg.tf
@@ -1,26 +1,35 @@
-resource "aws_security_group_rule" "allow_internal_traffic_8080" {
+resource "aws_security_group_rule" "allow_internal_traffic_80" {
   type                    = "ingress"
   security_group_id       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
-  from_port               = 8080
-  to_port                 = 8080
+  from_port               = 80
+  to_port                 = 80
   protocol                = "tcp"
-  cidr_blocks             = ["10.247.0.0/24", "10.248.0.0/24"]
+  cidr_blocks             = ["0.0.0.0/0", "0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "allow_internal_traffic_9090" {
-  type              = "ingress"
-  security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
-  from_port         = 9090
-  to_port           = 9090
-  protocol          = "tcp"
-  cidr_blocks       = ["10.247.0.0/24", "10.248.0.0/24"]
+resource "aws_security_group_rule" "allow_internal_traffic_443" {
+  type                    = "ingress"
+  security_group_id       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+  from_port               = 443
+  to_port                 = 443
+  protocol                = "tcp"
+  cidr_blocks             = ["0.0.0.0/0", "0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "allow_internal_traffic_1521" {
-  type              = "ingress"
-  security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
-  from_port         = 1521
-  to_port           = 1521
-  protocol          = "tcp"
-  cidr_blocks       = ["10.247.0.0/24", "10.248.0.0/24"]
-}
+# resource "aws_security_group_rule" "allow_internal_traffic_9090" {
+#   type              = "ingress"
+#   security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+#   from_port         = 9090
+#   to_port           = 9090
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0", "0.0.0.0/0"]
+# }
+
+# resource "aws_security_group_rule" "allow_internal_traffic_1521" {
+#   type              = "ingress"
+#   security_group_id = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+#   from_port         = 1521
+#   to_port           = 1521
+#   protocol          = "tcp"
+#   cidr_blocks       = ["10.247.0.0/24", "10.248.0.0/24"]
+# }
